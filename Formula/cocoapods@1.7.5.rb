0ba63723af96bd8dc6535e9a99b44d05f118341f
Jay Gauten <jgauten@vlocity.com>
2019-10-30 23:38:23 +0800

    force bottle

--- Formula/cocoapods@1.7.5.rb
+++ Formula/cocoapods@1.7.5.rb
@@ -2,10 +2,11 @@ class CocoapodsAT175 < Formula
   desc "Dependency manager for Cocoa projects"
   homepage "https://cocoapods.org/"
   url "https://github.com/CocoaPods/CocoaPods/archive/1.7.5.tar.gz"
-  sha256 "5a70fd60007975ce224824b1a8852ba9124faa6753061d0ecc024be4cebd6e8b"
+  sha256 "508e5f7a7566b3d05ec4e27417dc0a60bedc8e72618b31cb56713034e71337b9"
 
   bottle do
     cellar :any_skip_relocation
+    root_url "https://homebrew.bintray.com/bottles"
     sha256 "5a70fd60007975ce224824b1a8852ba9124faa6753061d0ecc024be4cebd6e8b" => :mojave
     sha256 "6d69384a2bfa22e24189cdaab477fa4a18703e7e5fed859454e822b4f28f4658" => :high_sierra
   end
